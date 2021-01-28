63833ede0c0a7054e9ccdab594413aba322bfe59
Christo <c.stasz@outlook.com>
2021-01-28 12:04:19 -0500

    BONUS

--- src/InsuranceCalculator.java
+++ src/InsuranceCalculator.java
@@ -1,21 +1,21 @@
+import java.util.HashMap;
+
 public class InsuranceCalculator {
 
     private InsuranceStrategy strategy;
+    private HashMap<Double, InsuranceStrategy> map = new HashMap<Double, InsuranceStrategy>();
 
     public double calculateInsurance(double income) {
-        if (income <= 10000) {
-            strategy = new InsuranceStrategyLow();
-            return strategy.calculate(income);
-        } else if (income <= 30000) {
-            strategy = new InsuranceStrategyMedium();
-            return strategy.calculate(income);
-        } else if (income <= 60000) {
-            strategy = new InsuranceStrategyHigh();
-            return strategy.calculate(income);
-        } else {
-            strategy = new InsuranceStrategyVeryHigh();
-            return strategy.calculate(income);
-        }
+        double adjustedIncome = Math.ceil(income/10000);
+
+        map.put(1.0, new InsuranceStrategyLow());
+        map.put(2.0, new InsuranceStrategyMedium());
+        map.put(3.0, new InsuranceStrategyMedium());
+        map.put(4.0, new InsuranceStrategyHigh());
+        map.put(5.0, new InsuranceStrategyHigh());
+        map.put(6.0, new InsuranceStrategyHigh());
+        strategy = map.getOrDefault(adjustedIncome, new InsuranceStrategyVeryHigh());
+        return strategy.calculate(income);
     }
 
 
